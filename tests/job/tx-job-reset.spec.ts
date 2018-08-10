3bcda86959972b722a9a1877f9029fc50d636ca7
tsemach <tsemach.mizrachi>
2018-08-10 17:11:45 +0300

    add job undo

--- tests/job/tx-job-reset.spec.ts
+++ tests/job/tx-job-reset.spec.ts
@@ -14,7 +14,39 @@ const logger = createLogger('Job-Test');
 
 describe('Job Class', () => {
 
-  /**
-   */
+  it('check running C1-C2-C3 job chain', async () => {
+    
+    let C1 = new C1Component();
+    let C2 = new C2Component();
+    let C3 = new C3Component();
+    
+    let job = new TxJob(); // or create througth the TxJobRegistry
 
+    job.add(TxMountPointRegistry.instance.get('GITHUB::GIST::C1'));
+    job.add(TxMountPointRegistry.instance.get('GITHUB::GIST::C2'));
+    job.add(TxMountPointRegistry.instance.get('GITHUB::GIST::C3'));
+
+    job.execute(new TxTask(
+      'create',
+      '',
+      {something: 'more data here'})
+    );            
+    console.log("end of first execution");
+    
+    job.reset();
+    job.execute(new TxTask(
+      'create',
+      '',
+      {something: 'more data here'})
+    );            
+    console.log("end of second execution");    
+
+    job.getIsCompleted().subscribe(
+      (data) => {
+        console.log('job.getIsCompleted: complete running all tasks - data:' + JSON.stringify(data, undefined, 2));
+        expect(data).to.equal(3);        
+      }
+    );
+    
+  });
 });
--- tests/job/tx-job-undo.spec.ts
+++ tests/job/tx-job-undo.spec.ts
@@ -4,7 +4,7 @@ import {expect} from 'chai';
 
 import {TxMountPointRegistry} from '../../src/tx-mountpoint-registry';
 import {TxTask} from '../../src/tx-task';
-import {TxJob} from '../../src/tx-job';
+import {TxJob, TxDirection} from '../../src/tx-job';
 
 import {C1Component} from './C1.component';
 import {C2Component} from './C2.component';
@@ -17,4 +17,27 @@ describe('Job Class', () => {
   /**
    */
 
+  it('check C1-C2-C3 undo ', () => {
+    let C1 = new C1Component();
+    let C2 = new C2Component();
+    let C3 = new C3Component();
+
+    let job = new TxJob();
+
+    job.add(TxMountPointRegistry.instance.get('GITHUB::GIST::C1'));
+    job.add(TxMountPointRegistry.instance.get('GITHUB::GIST::C2'));
+    job.add(TxMountPointRegistry.instance.get('GITHUB::GIST::C3'));
+    
+    job.execute(new TxTask(
+      'create',
+      '',
+      {something: 'more data here'})
+    );            
+    console.log("end of first execution");
+
+    job.undo(new TxTask(
+      'create',
+      '',
+      {something: 'more data here'}));
+  }); 
 });
