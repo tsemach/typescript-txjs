76e78ef6e964138e11a14d5c2a56587f6f66d203
tsemach <tsemach.mizrachi>
2018-09-16 22:04:16 +0300

    update typescript-rabbitmq

--- src/tx-mountpoint-registry.ts
+++ src/tx-mountpoint-registry.ts
@@ -6,13 +6,6 @@ const logger = createLogger('MountPointRegistry');
 import { TxRegistry } from './tx-registry';
 import { TxMountPoint } from './tx-mountpoint';
 import { TxMountPointRxJS } from './tx-mount-point-rxjs';
-import { TxQueuePoint } from "./tx-queuepoint";
-import { TxRoutePoint } from "./tx-routepoint";
-import { Container } from "inversify";
-import { TxTYPES } from "./tx-injection-types";
-import { TxConnector } from "./tx-connector";
-import { TxConnectorRabbitMQ } from "./tx-connector-rabbitmq";
-import { TxConnectorExpress } from "./tx-connector-express";
 
 export class TxMountPointRegistry extends TxRegistry<TxMountPoint, string | Symbol> {
   private static _instance: TxMountPointRegistry;
