96f58b1dfd78308a4e2d2551d3c891c1c974eea3
Tsemach Mizrachi <tsemach.mizrachi@gmail.com>
2019-04-06 10:57:29 +0300

    fic job.js

--- examples/models/job.js
+++ examples/models/job.js
@@ -1,3 +1,22 @@
+
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Jobs = sequelize.define('Jobs', {
+    jobId: {
+               type: DataTypes.STRING, 
+               allowNull: false, 
+               primaryKey: true, 
+        },
+    name: DataTypes.STRING,
+    data: DataTypes.JSON
+  }, {});
+  Jobs.associate = function(models) {
+    // associations can be defined here
+     };
+      return Jobs;
+      };
+   
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
