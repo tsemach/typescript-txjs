dbb9d915baccd628cdacdbd2b812cb0f3d189934
tsemach <tsemach.mizrachi>
2019-04-06 10:41:41 +0300

    Add postgres presist

---
+++ examples/config/postgres/config.js
@@ -0,0 +1,25 @@
+
+
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "examples",
+    "host": "localhost",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "examples",
+    "host": "localhost",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "examples",
+    "host": "localhost",
+    "dialect": "postgres"
+  }
+};
